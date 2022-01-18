105e5accaea2f8a42cf12150e4a6ff611ccd85d8
Gerome Mandapat <42403342+97gerome@users.noreply.github.com>
2022-01-19 02:29:01 +0800

    Removed Text import on line 2

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { SetStateAction } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
 import AccountButton from './AccountButton';
 
